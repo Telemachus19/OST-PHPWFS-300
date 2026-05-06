235598f7056389e1686a4be375d8e3dc38335518
Telemachus19 <aashrafmohamed84@gmail.com>
2026-05-06 10:36:46 +0300

    feat: add image column to posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +16,7 @@ class Post extends Model
         'user_id',
         'title',
         'content',
+        'image',
     ];
 
     public function user(): BelongsTo
